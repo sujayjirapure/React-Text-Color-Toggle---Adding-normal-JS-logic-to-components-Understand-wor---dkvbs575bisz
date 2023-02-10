9ddb5142316c823e2273dc291a8f19dcb73f7f36
sujay <jirapuresujay@gmail.com>
2023-02-11 00:59:36 +0530

    done 1

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,23 @@ import React, {useEffect, useState} from 'react'
 import '../styles/App.css';
 
 const App = () => {
-//code here 
+  const [Show ,setShow] = useState(false);
+
+  const clickfunc = () => {
+    setShow(!Show);
+  }
+
   return (
     <div id="main">
-      <p className={} >Newton School</p>
-      <button id='button' onClick={}>Change Style</button>
+      {Show ?
+      <p className="redColor">Newton School</p>
+      :
+      <p className="blueColor">Newton School</p>
+      }
+      <button id='button' onClick={clickfunc}>Change Style</button>
     </div>
-  )
-}
+  );
+};
 
 
 export default App;
